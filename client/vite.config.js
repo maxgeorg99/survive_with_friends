af2c8925a6e7f5e9851be9553477d61af8795916
Drew Wright <pandacodemaster@gmail.com>
2025-06-08 18:46:29 +0200

    trying to fix deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ export default defineConfig({
   server: {
     port: 8080 // Or any port you prefer
   },
+  preview: {
+    host: true, // Allow external connections
+    allowedHosts: 'all' // Allow all hosts (including Render's domain)
+  },
   build: {
     outDir: 'dist'
   },
